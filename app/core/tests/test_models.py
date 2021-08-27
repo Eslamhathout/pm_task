747196ff3b3ca296600ee9249964413d00224f4b
Eslamhathout <i.a.hathout@gmail.com>
2021-08-27 21:42:01 +0200

    create admin user configurations

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -34,7 +34,7 @@ class ModelTests(TestCase):
 
     def test_create_new_superuser(self):
         """Test creating a new superuser"""
-        email = 'eslam@PAYMOB.COM'
+        email = 'eslamh@PAYMOB.COM'
         password = 'testpass'
 
         user = get_user_model().objects.create_superuser(
